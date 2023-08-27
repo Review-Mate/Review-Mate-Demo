fc4a0d425ea73b9827fe3e13e4d933e238e88387
hyeonjin25 <shj41738866@gmail.com>
2023-08-27 18:23:04 +0900

    design: 호텔 사진 grid 부분 반응형으로 변경 (relate to: #3)

--- components/detail/Top/LeftInfo.tsx
+++ components/detail/Top/LeftInfo.tsx
@@ -5,26 +5,25 @@ export default function LeftInfo() {
   const nums = [2, 3, 4, 5, 6, 7];
   return (
     <div>
-      <div className='flex flex-col'>
+      <div className='grid grid-rows-5 grid-cols-6 gap-1'>
         <Image
-          className='round-10 mb-[2px]'
+          className='w-full row-span-4 col-span-6'
           src={'/hotel1.png'}
           alt='hotel1'
           width={535}
           height={374}
         />
-        <div className='grid grid-cols-6 gap-[2px]'>
-          {nums.map((num) => (
-            <Image
-              key={num}
-              className='round-10 flex-1'
-              src={`/hotel${num}.png`}
-              alt='hotel2'
-              width={85}
-              height={85}
-            />
-          ))}
-        </div>
+
+        {nums.map((num) => (
+          <Image
+            key={num}
+            className='w-full round-10 flex flex-1'
+            src={`/hotel${num}.png`}
+            alt='hotel2'
+            width={85}
+            height={85}
+          />
+        ))}
       </div>
     </div>
   );
