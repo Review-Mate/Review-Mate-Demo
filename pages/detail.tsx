f9a3cc02c3bf366d751c3837a3d804c35a6b4fc1
hyeonjin25 <shj41738866@gmail.com>
2023-08-27 18:21:18 +0900

    feat: 상세페이지 room 정보 부분 ui 추가 (related to: #3)

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -1,16 +1,18 @@
 import Seo from '@/components/Seo';
+import Rooms from '@/components/detail/Rooms/Index';
 import TopInfo from '@/components/detail/Top/Index';
 import Image from 'next/image';
 import React from 'react';
 
 type Props = {};
 
-export default function detail({}: Props) {
+export default function Detail({}: Props) {
   const nums = [2, 3, 4, 5, 6, 7];
   return (
     <div className='h-full'>
       <Seo title='ReviewMate | Home' />
       <TopInfo />
+      <Rooms />
       <iframe
         src='https://main.d33vkfpm9flfhs.amplifyapp.com/review/list'
         className='w-full h-full overflow-y-hidden'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Seo from '@/components/Seo';
+import Detail from './detail';
 
 export default function Home() {
   return (
     <main className='h-full'>
+      <Detail />
     </main>
   );
 }
