1e1c178634699f03b0336585728ab63d3a7bae73
sohyeonjin <65444249+hyeonjin25@users.noreply.github.com>
2023-08-31 16:22:44 +0900

    Revert "feat: 상세페이지 ui 완성 및 리뷰 위젯 추가 (related to: #3 #4)"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head />
-      <meta charSet='utf-8'></meta>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
