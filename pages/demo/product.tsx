a70c5d532914ee0d5618f4381ae8e81a5e6ac6d8
hyeonjin25 <shj41738866@gmail.com>
2023-09-02 10:22:58 +0900

    feat: 리뷰 작성 위젯(iframe) 추가 (related to: #11) 기존 자식과의 message 프로세스 -> messageToChild hook으로 변경

--- pages/demo/product.tsx
+++ pages/demo/product.tsx
@@ -1,39 +1,13 @@
 import Seo from '@/components/Seo';
 import Rooms from '@/components/detail/Rooms/Index';
 import TopInfo from '@/components/detail/Top/Index';
-import Image from 'next/image';
+import useMessageToChild from '@/hooks/useMessageToChild';
 import React, { useEffect } from 'react';
 
 type Props = {};
 
 export default function Product({}: Props) {
-  const iframeRef = React.useRef<HTMLIFrameElement | null>(null);
-
-  React.useEffect(() => {
-    const iframe = iframeRef.current;
-    if (!iframe) return undefined;
-
-    const listener = (e: MessageEvent) => {
-      if (!e.data.type) return;
-      if (e.data.type === 'height') {
-        console.log('자식으로 부터 온 height', e.data.message);
-        iframe.style.height = `${e.data.message}px`;
-      }
-    };
-
-    window.addEventListener('message', listener);
-
-    if (iframe && iframe.contentWindow) {
-      iframe.contentWindow?.postMessage(
-        { type: 'loaded', message: 'on loaded!' },
-        '*'
-      );
-    }
-
-    return () => {
-      window.removeEventListener('message', listener);
-    };
-  }, []);
+  const { iframeRef } = useMessageToChild();
 
   return (
     <div className='h-full'>
--- pages/demo/review/write.tsx
+++ pages/demo/review/write.tsx
@@ -1,16 +1,23 @@
 import Seo from '@/components/Seo';
 import WritePageTopInfo from '@/components/write/Top';
 import { WIDGET_API } from '@/config/api';
+import useMessageToChild from '@/hooks/useMessageToChild';
 import React from 'react';
 
 type Props = {};
 
 export default function WritePage({}: Props) {
+  const { iframeRef } = useMessageToChild();
+
   return (
     <div>
       <Seo title='ReviewMate | 리뷰작성' />
       <WritePageTopInfo />
-      <iframe src={`${WIDGET_API}/review/write`} className='w-full' />
+      <iframe
+        ref={iframeRef}
+        src={`${WIDGET_API}/review/write`}
+        className='w-full'
+      />
     </div>
   );
 }
