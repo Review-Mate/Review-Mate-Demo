12dae4923104a101b852b944cf440cb5f7218682
hyeonjin25 <shj41738866@gmail.com>
2023-09-03 11:59:22 +0900

    feat: api 추가

--- config/api.tsx
+++ config/api.tsx
@@ -1,7 +1,7 @@
 const WIDGET_DEV_API = 'http://localhost:3001';
-const WIDGET_PROD_API = 'https://main.d33vkfpm9flfhs.amplifyapp.com';
+const WIDGET_PROD_API = 'https://widget.reviewmate.co.kr';
 
-let mode = 'dev';
-// let mode = 'prod'
+// let mode = 'dev';
+let mode = 'prod'
 
 export const WIDGET_API = mode === 'prod' ? WIDGET_PROD_API : WIDGET_DEV_API;
