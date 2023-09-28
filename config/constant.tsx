48688427b081dc34be59237d49387731830679f5
hyeonjin25 <shj41738866@gmail.com>
2023-09-28 18:45:30 +0900

    fix: constant.tsx를 ignore해서 생긴 에러 해결, 남아있던 recoil 관련 코드 제거

---
+++ config/constant.tsx
@@ -0,0 +1,8 @@
+// 데모 리뷰 조회용 상품 id
+export const PRODUCT_ID = 'PRODUCT-0001';
+
+// 리뷰메이트 서비스를 사용하는 파트너사 도메인
+export const PARTNER_DOMAIN = 'goodchoice.kr';
+
+// 데모를 위한 리뷰메이트 서비스 API URL
+export const REVIEW_MATE_URL = 'http://test.api.reviewmate.co.kr:8080';
--- state/reservationState.tsx
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const reservationIdState = atom({
-  key: 'reservationIdState', 
-  default: '', 
-});
