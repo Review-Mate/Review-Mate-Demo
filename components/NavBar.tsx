03e2b2e90c7fdc303638916f2392b1ef1b8ad526
hyeonjin25 <shj41738866@gmail.com>
2023-10-04 00:12:24 +0900

    design: 반응형에 따른 레이아웃 배치

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,19 +4,17 @@ import React from 'react';
 import logo from '@/public/images/logo.svg';
 import user from '@/public/images/user.svg';
 
-type Props = {};
-
-export default function NavBar({}: Props) {
+export default function NavBar() {
   return (
     <nav className='h-[70px] flex items-center justify-between mb-12'>
       <Link href='/'>
         <Image src={logo} alt='로고' width={115} height={40} />
       </Link>
-      <div className='w-3/5 flex gap-16'>
-        <div className='w-25'>
+      <div className='w-3/5 flex'>
+        <div className='w-23 md:w-25 mr-[10%]'>
           <Link href='/demo/product'>리뷰 목록 체험</Link>
         </div>
-        <div className='w-25'>
+        <div className='w-23 md:w-25'>
           <Link href='/demo/beforeReview'>리뷰 작성 체험</Link>
         </div>
       </div>
