f7d214986578d107bed33c9683fc424396cb6bca
hyeonjin25 <shj41738866@gmail.com>
2023-10-23 01:06:53 +0900

    feat: 검색엔진 최적화 위한 meta 태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,13 @@ export default function Document() {
           httpEquiv='Content-Security-Policy'
           content='upgrade-insecure-requests'
         />
+        <meta name="Keywords" content="리뷰, 여행, 리뷰 관리, 여행 리뷰, 리뷰메이트" />
+        <meta
+          name='Description'
+          content='여행상품의 구매 전환율을 상승시키는 리뷰 통합 관리 플랫폼, 리뷰메이트'
+        />
+        <meta charSet='utf-8' />
       </Head>
-      <meta charSet='utf-8'></meta>
       <body>
         <Main />
         <NextScript />
