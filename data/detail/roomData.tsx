268915ce90479b86edc08a15f51e7ee12df4b01c
hyeonjin25 <shj41738866@gmail.com>
2023-10-31 20:47:05 +0900

    feat: 가격 정보 업데이트

--- data/detail/roomData.tsx
+++ data/detail/roomData.tsx
@@ -1,26 +1,26 @@
 export const roomList = [
   {
-    name: '스탠다드',
+    name: '프리미엄 스탠다드',
     image: '/images/room_single.webp',
-    price: 127000,
+    price: 220000,
     options: ['조식 미포함', '싱글베드 1개', '객실크기 45㎡', '금연객실'],
   },
   {
     name: '프리미엄 트원',
     image: '/images/room_premium_twin.webp',
-    price: 127000,
+    price: 260000,
     options: ['조식 포함', '트윈베드 1개', '객실크기 50㎡', '금연객실'],
   },
   {
     name: '가든 킹',
     image: '/images/room_king_garden_view.webp',
-    price: 127000,
+    price: 350000,
     options: ['조식 포함', '킹베드 1개', '객실크기 72㎡', '금연객실'],
   },
   {
     name: '프리미엄 킹',
     image: '/images/room_premium_king.webp',
-    price: 127000,
+    price: 370000,
     options: ['조식 포함', '킹베드 1개', '객실크기 50㎡', '금연객실'],
   },
 ];
