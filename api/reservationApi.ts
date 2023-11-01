c8d4f89a79a882eb147316bc43e9b7c9221bd9f0
hyeonjin25 <shj41738866@gmail.com>
2023-11-01 17:34:46 +0900

    refactor: 기존 복잡한 리뷰 예약 코드를 분

---
+++ api/reservationApi.ts
@@ -0,0 +1,15 @@
+import { PARTNER_DOMAIN, REVIEW_MATE_URL } from '@/config/constant';
+import axios from 'axios';
+
+export async function createReservation(reservationData: FormData) {
+  try {
+    const response = await axios.post(
+      `${REVIEW_MATE_URL}/api/client/v1/${PARTNER_DOMAIN}/products/travel/single/reservations`,
+      reservationData
+    );
+    return response.data;
+  } catch (error) {
+    alert('예약에 실패했습니다. 다시 시도해주세요.');
+    throw error;
+  }
+}
