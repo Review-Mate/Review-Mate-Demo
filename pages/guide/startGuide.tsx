d19c6eaa995961911ac81cc3c0850bedbee429f0
hyeonjin25 <shj41738866@gmail.com>
2023-11-01 22:24:40 +0900

    design: 개발자문서 대제목 추가

--- pages/guide/startGuide.tsx
+++ pages/guide/startGuide.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import React, { useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { githubGist } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
@@ -8,10 +7,15 @@ import { codeStartExample } from '@/data/detail/guideCodeData';
 export default function Insert() {
   return (
     <>
-      <h1 className='text-heading font-bold mb-5'>리뷰메이트 위젯 삽입 예제</h1>
-      <h2 className='text-body2 mb-3'>
-      간단한 예제를 통해 리뷰메이트 위젯 삽입 방법을 설명합니다.
+      <div className='absolute left-0 w-full text-heading font-bold bg-gray06 -mt-9 py-5 border-b-2 border-b-gray04'>
+        <h1 className='px-[5%] xl:px-[15%]'>리뷰메이트 삽입 가이드</h1>
+      </div>
+      <h2 className='text-title font-bold mt-[130px] mb-5'>
+        리뷰메이트 위젯 삽입 예제
       </h2>
+      <h3 className='text-body2 mb-4'>
+        간단한 예제를 통해 리뷰메이트 위젯 삽입 방법을 설명합니다.
+      </h3>
       <div className='relative'>
         <CodeHighlight code={codeStartExample} />
         <CopyButton code={codeStartExample} />
