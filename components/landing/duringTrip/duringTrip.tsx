2d8366ade86195ff42a28cd34e131f018b9b5bc5
hyeonjin25 <shj41738866@gmail.com>
2023-11-23 00:06:08 +0900

    feat: 챗봇 페이지 최신 디자인 적용 (related to: #44)

--- components/landing/duringTrip/duringTrip.tsx
+++ components/landing/duringTrip/duringTrip.tsx
@@ -17,23 +17,26 @@ export default function DuringTrip() {
     >
       <Line title='여행 중' iconSrc='/icons/line4.svg' top={'top-[445px]'} />
       <div className={`${layoutWidth}  ${flexCol}`}>
-      <h1 className='mb-4 font-bold text-title'>꼼꼼한 여행관리, 챗봇메이트</h1>
-      <h2 className='mb-16 text-center text-body2 text-gray01'>
-        선택된 여행상품의 만족도 및 응대 데이터 관련하여 피드백을 주고 받을 수
-        있습니다
-      </h2>
-      <div className='flex flex-row justify-between w-full mb-16'>
-        <Step1 />
-        <Step2 />
-        <Step3 />
+        <h1 className='mb-4 font-bold text-title'>
+          꼼꼼한 여행관리, 챗봇메이트
+        </h1>
+        <h2 className='mb-16 text-center text-body2 text-gray01'>
+          선택된 여행상품의 만족도 및 응대 데이터 관련하여 피드백을 주고 받을 수
+          있습니다
+        </h2>
+        <div className='flex flex-row justify-between w-full mb-16'>
+          <Step1 />
+          <Step2 />
+          <Step3 />
+        </div>
+        <GreenButton
+          title='챗봇메이트 체험'
+          iconSrc='/icons/chat.svg'
+          onClick={() => {
+            router.push('/demo/during-trip/chatBot');
+          }}
+        />
       </div>
-      <GreenButton
-        title='챗봇메이트 체험'
-        iconSrc='/icons/chat.svg'
-        onClick={() => {
-          router.push('/demo/during-trip/beforeChatBot');
-        }}
-      /></div>
     </div>
   );
 }
