a6941a9e925690544e0bdb93ccda62864345d749
hyeonjin25 <shj41738866@gmail.com>
2023-11-24 16:24:23 +0900

    feat: 리뷰메이트 삽입 가이드 업데이트

--- data/detail/guideCodeData.tsx
+++ data/detail/guideCodeData.tsx
@@ -6,16 +6,36 @@ export const codeStartExample = `<!DOCTYPE html>
     <title>리뷰메이트 위젯 테스트하기</title>
   </head>
   <body>
-    <div>안뇽</div>
+    <div>상품 페이지 입니다</div>
+
+    <!--리뷰 메이트 위젯 -->
     <div
       id="review-mate-widget"
       domain="goodchoice.kr"
       product-id="PRODUCT-0001"
       type="list"
-    ></div>
+    />
+
+    <!--리뷰 메이트 위젯 스크립트 -->
     <script
       type="text/javascript"
-      src="http://localhost:3000/script/widgetScript.js"
+      src="https://widget.reviewmate.co.kr/script/widgetScript.js"
     ></script>
+    
   </body>
 </html>`;
+
+export const listWidgetCode = `<div
+  id="review-mate-widget"
+  domain="goodchoice.kr"
+  product-id="PRODUCT-0001"
+  type="list"
+/>
+`;
+export const writeWidgetCode = `<div
+  id="review-mate-widget"
+  domain="goodchoice.kr"
+  reservation-id="1"
+  type="write"
+/>
+`;
